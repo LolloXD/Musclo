ca3f511b5fcaf4c4519a2a2fa259c0e421ad54bb
LolloXD <93399640+LolloXD@users.noreply.github.com>
2026-04-15 15:56:52 +0200

    Commit iniziale

---
+++ app/src/main/java/com/example/musclo/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.musclo
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.os.postDelayed
+import com.example.musclo.ui.theme.MuscloTheme
+import kotlinx.coroutines.delay
+import java.util.logging.Handler
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Splash screen
+
+        android.os.Handler().postDelayed({
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish() // Se no si può tornare indietro sulla schermata, che non va bene
+        }, (3000)
+        )
+
+
+
+    }
+}
