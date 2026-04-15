b1283c8b0486be643f1044776b34a2e7b86f142d
LolloXD <93399640+LolloXD@users.noreply.github.com>
2026-04-15 17:50:10 +0200

    Aggiunta della schermata di login

--- app/src/main/java/com/example/musclo/MainActivity.kt
+++ app/src/main/java/com/example/musclo/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.musclo
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -18,14 +20,20 @@ import kotlinx.coroutines.delay
 import java.util.logging.Handler
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var usernameInput : EditText
+    lateinit var passwordInput : EditText
+    lateinit var loginBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         // Splash screen
 
         android.os.Handler().postDelayed({
-            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(intent)
             finish() // Se no si può tornare indietro sulla schermata, che non va bene
         }, (3000)
@@ -33,5 +41,8 @@ class MainActivity : ComponentActivity() {
 
 
 
+
+
     }
 }
+
