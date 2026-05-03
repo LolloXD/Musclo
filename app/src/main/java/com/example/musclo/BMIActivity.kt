d4456faea3971201974e00802ca0f7bc456b8362
Bryan2088 <baldari.bryan.22@itisriva.edu.it>
2026-05-03 15:00:35 +0200

    Aggiunta dei commenti (in maniera specifica)

--- app/src/main/java/com/example/musclo/BMIActivity.kt
+++ app/src/main/java/com/example/musclo/BMIActivity.kt
@@ -16,25 +16,31 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.musclo.ui.theme.MuscloTheme
 
-class BMIActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bmi)
+class BMIActivity : ComponentActivity() //Creando una nuova activity che estende ComponentActivity
+{
+    override fun onCreate(savedInstanceState: Bundle?)  //Metodo che parte alla creazione dell'activity
+    {
+        super.onCreate(savedInstanceState) //Inizializza l'activity
+        setContentView(R.layout.activity_bmi) //Dice quale grafica usare
 
+        //Elementi ui e variabili
         val etPeso = findViewById<EditText>(R.id.etPeso)
         val etAltezza = findViewById<EditText>(R.id.etAltezza)
         val btnCalcola = findViewById<Button>(R.id.btnCalcola)
         val txtRisultato = findViewById<TextView>(R.id.txtRisultato)
 
-        btnCalcola.setOnClickListener {
+        btnCalcola.setOnClickListener {   //Codice che viene eseguito al click del bottone
 
-            val peso = etPeso.text.toString().toDoubleOrNull()
-            val altezzaCm = etAltezza.text.toString().toDoubleOrNull()
+            val peso = etPeso.text.toString().toDoubleOrNull()  //prende il testo scritto, lo converte in stringa e prova a convertirlo in numero
+            val altezzaCm = etAltezza.text.toString().toDoubleOrNull() //prende il testo scritto, lo converte in stringa e prova a convertirlo in numero
 
-            if (peso != null && altezzaCm != null) {
+            if (peso != null && altezzaCm != null) //Controlla che i valori siano validi
+            {
 
-                val altezzaMetri = altezzaCm / 100
-                val bmi = peso / (altezzaMetri * altezzaMetri)
+                val altezzaMetri = altezzaCm / 100 //Converte la cm in metri
+                val bmi = peso / (altezzaMetri * altezzaMetri) //Calcola il BMI
+
+                //Decide la categoria in base al BMI
 
                 val categoria = when {
                     bmi < 18.5 -> "Sottopeso"
@@ -43,10 +49,10 @@ class BMIActivity : ComponentActivity() {
                     else -> "Obesità"
                 }
 
-                txtRisultato.text = "BMI: %.2f ($categoria)".format(bmi)
+                txtRisultato.text = "BMI: %.2f ($categoria)".format(bmi)  //Mostra il risultato(due cifre dopo la virgola e la categoria)
 
             } else {
-                txtRisultato.text = "Inserisci valori validi"
+                txtRisultato.text = "Inserisci valori validi" //In caso di valori non validi
             }
         }
     }
